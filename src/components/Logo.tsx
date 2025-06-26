89cc8b9681e097eacc0f584f6e934506fd187720
[dyad] <corazzisamuel4@gmail.com>
2025-06-26 12:19:25 +0200

    [dyad] Ingrandimento del logo - wrote 1 file(s)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Logo: React.FC = () => {
   return (
     <div className="absolute top-4 left-4 sm:top-6 sm:left-6 z-50">
-      <img src="https://i.postimg.cc/hjC0gtTz/Corazzi-upscaled.png" alt="Corazzi Logo" className="w-[80px] sm:w-[100px] h-auto" />
+      <img src="https://i.postimg.cc/hjC0gtTz/Corazzi-upscaled.png" alt="Corazzi Logo" className="w-[100px] sm:w-[120px] h-auto" />
     </div>
   );
 };
